c0cccdf3b7e27b105abbc3222041365b78e76ae3
LaboratorioInnovacion <psiconervio@gmail.com>
2025-05-22 20:43:07 -0300

    asda

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Asegúrate de que esté configurado correctamente
+
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
