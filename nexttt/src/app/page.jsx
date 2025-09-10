740f0c81ad5695439d44027647382cdf919de261
LaboratorioInnovacion <psiconervio@gmail.com>
2025-09-10 18:17:18 -0300

    asda

--- nexttt/src/app/page.jsx
+++ nexttt/src/app/page.jsx
@@ -97,7 +97,7 @@ const Page = () => {
           </Link>
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-4">
+      <div className="grid grid-cols-1 lg:grid-cols-1 gap-4">
         <StatCard
           cantidad={(allemprendimientos || []).length}
           empre={undefined}
